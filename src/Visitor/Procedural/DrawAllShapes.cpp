0335a6899d54c8d894c454c1741c49abbd3f8a8f
ryomashita <86085692+ryomashita@users.noreply.github.com>
2024-02-02 11:15:43 +0900

    add comment

--- src/Visitor/Procedural/DrawAllShapes.cpp
+++ src/Visitor/Procedural/DrawAllShapes.cpp
@@ -5,6 +5,12 @@
 #include "DrawSquare.h"
 #include "Square.h"
 
+// Visitor Before (Procedural approach)
+// 列挙型ShareTypeによりShapreサブクラスを判別する。
+// 新たなShareサブクラス(ShareType)を追加するたび、switch文を修正する必要があり、
+// 実質的に保守不可能といえる。
+// 主たる原因は、全ての型と処理が列挙型に直接依存し、結びついている点にある。
+
 namespace visitor {
 double GetAllArea(std::vector<std::shared_ptr<Shape>> const& shapes) {
   double accum = 0;
